68321d172be2256e9ecb171a314832010f692df6
HSNBDES33\eocariz <eocariz@hasar.com>
2017-12-21 15:41:00 -0300

    versión de "store" mobx con @action

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
-import { observable } from 'mobx';
+import { observable, action } from 'mobx';
 
+/*
 const appState = observable({
     grammar: null, 
     textCode: '',
@@ -21,5 +22,27 @@ appState.setGrammar = grammar => {
     appState.grammar = grammar;
     appState.parseResult = [];
 }
+*/
 
+const appState = new class AppState {
+    @observable grammar = null;
+    @observable textCode = '';
+    @observable parseResult = [];
+    @observable grammarList = ['Hello', 'ETMRules', 'arithmetic', 'other'];
+    @observable hasParseResult = false;
+
+    @action setParseLine = line => {
+        appState.parseResult = [...appState.parseResult, line];
+        appState.hasParseResult = true;
+    }
+
+    @action setCode = code => {
+        appState.textCode = code;
+    }
+
+    @action setGrammar = grammar => {
+        appState.grammar = grammar;
+        appState.parseResult = [];
+    }
+}
 export default appState;
