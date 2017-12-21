5146b161cb5788a6b89ca7ea459645b489c6015e
HSNBDES33\eocariz <eocariz@hasar.com>
2017-12-21 15:06:58 -0300

    agrego herramientas de debug para mobx

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import CodeEditor from './containers/CodeEditor';
 import appState from './store';
 import { PropTypes } from 'prop-types';
 import { observer } from 'mobx-react';
+import DevTools from 'mobx-react-devtools';
 
 @observer
 class App extends Component {
 
   render() {
 
-    const { hasParseResult } = appState.hasParseResult;
+    const { hasParseResult } = appState;
     return (
       <div className={`App ${hasParseResult && 'with-result'}`}>
+        <DevTools></DevTools>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">{!hasParseResult && 'Bienvenido a '}Lexxer</h1>
